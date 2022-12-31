ab96908515fb239d91148b7b596c01759f81c025
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2022-12-31 18:29:33 +0600

    audio added

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-document.querySelector("button").addEventListener("click", function() {
+var buttonLength = document.querySelectorAll("button").length;
 
-    alert("I got clicked");
-})
+for (var i=0; i<buttonLength; i++)
+{
+    document.querySelectorAll("button")[i].addEventListener("click", function() {
+
+        // console.log(this);
+
+        var audio = new Audio('sounds/tom-1.mp3');
+        audio.play();
+    
+    // this.classList.add("hit");    
+    })
+}
